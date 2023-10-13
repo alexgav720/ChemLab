f948bcdddcb6b2a4afa4b764f70f4ea3488b4319
alexgav720 <77044188+alexgav720@users.noreply.github.com>
2023-10-13 11:46:36 +0300

    Add files via upload

--- chem/js/main.js
+++ chem/js/main.js
@@ -1,12 +1,20 @@
 let s = Snap('#main');
-
+let paper = Snap(200,200);
 let square = s.rect(0,0,160,160);
-let square2 = s.rect(0,0,160,160);
+
+paper.drag();
 square.drag();  
 square2.drag();  
-s.load("untitled.svg",function(){})
 
-s.attr({
+Snap.load("/untitled.svg",function(f){
+    // let paper = Snap('#star');   
+    paper.append(f)
+
+
+})
+
+s.append(paper)
+square.attr({
     fill: 'green'
 })
 
