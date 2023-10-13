38b11d903b8c344f247c373a519792e34c912a74
alexgav720 <77044188+alexgav720@users.noreply.github.com>
2023-10-13 16:42:00 +0300

    Add files via upload

--- chem/js/main.js
+++ chem/js/main.js
@@ -26,22 +26,58 @@ let s = Snap('#main');
 //     element.attr({ cursor: 'move' });
 // }
 
+s.image("laba.jpg")
 
-let  addcolb = function(){Snap.load("/book.svg",function(f){
-    let book = f.select("#in"),
-        out = f.select("#out"),
-        colb1 = s.group(book,out)
+function isOverlap(element1, element2) {
+    var bbox1 = element1.getBBox();
+    var bbox2 = element2.getBBox();
+
+    // Check for overlap by comparing bounding boxes
+    return (
+        bbox1.x < bbox2.x + bbox2.width &&
+        bbox1.x + bbox1.width > bbox2.x &&
+        bbox1.y < bbox2.y + bbox2.height &&
+        bbox1.y + bbox1.height > bbox2.y
+    );
+}
+let petry;
+let petryin;
+Snap.load("/colb.svg",function(f){
+        petryin = f.select("#in")
+        petryout = f.select("#out")
+        petry = s.group(petryin,petryout)
+    s.append(petry)
+    petry.drag();
+    petryin.attr({
+        fill:"green"
+    })
+}, s);
+
+let  addcolb = function(){Snap.load("/colb.svg",function(f){
+    let colbin = f.select("#in"),
+        colbout = f.select("#out"),
+        colb1 = s.group(colbin,colbout)
     s.append(colb1)
     colb1.drag();
-    out.attr({
+    colbin.attr({
         fill:"red"
     })
 
-    out.animate({fill:"blue"},3000,mina.ease);
+    colbin.animate({fill:"blue"},3000,mina.ease);
 
+    colb1.mouseup(function() {
+        if(isOverlap(colb1,petry)){
+           petryin.animate({
+                fill: "yellow"
+            },3000,mina.ease)
+        }
+        colb1.attr({
+            cx:10,
+            cy:10})
+    })
 }, s);
 }
-for(let i =0;i<3;i++){
+for(let i =0;i<1;i++){
     addcolb();
 }   
 
