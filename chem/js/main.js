9c38e333a7addbf6b78d58b7a2f2c12e97ca4226
alexgav720 <77044188+alexgav720@users.noreply.github.com>
2023-10-13 11:17:29 +0300

    Add files via upload

--- chem/js/main.js
+++ chem/js/main.js
@@ -1,5 +1,20 @@
 let s = Snap('#main');
 
+let square = s.rect(0,0,160,160);
+let square2 = s.rect(0,0,160,160);
+square.drag();  
+square2.drag();  
+s.load("untitled.svg",function(){})
+
 s.attr({
     fill: 'green'
 })
+
+
+
+// Snap.load(img,function(f){
+//     g = f.selectAll("polygon");
+//     s.append(g);
+// })
+
+console.log(s);
