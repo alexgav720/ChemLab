d1cc7d544c229ae1521475f27c2b4d6efb880c0b
alexgav720 <77044188+alexgav720@users.noreply.github.com>
2023-10-13 14:44:40 +0300

    Add files via upload

--- chem/js/main.js
+++ chem/js/main.js
@@ -1,22 +1,52 @@
 let s = Snap('#main');
-let square = s.rect(0,0,160,160);
+// s.append(paper)
+// let img = s.image("/untitled.svg",10,10,100,100)
+// img.drag()
 
-square.drag();  
 
-Snap.load("/book.svg",function(f){
-    g = f.selectAll("#book");
-    s.append(g);
-    
-    
-})
-let paper = Snap("#book");
-s.append(paper)
-paper.drag();
+// // potom
+
+// let rect = paper.rect(0,0,100,100)
+// let group = paper.group(img,rect);
+
+// Reusable drag function
+// function makeDraggable(element) {
+//     element.drag(
+//         function (dx, dy, x, y) {
+//             this.attr({
+//                 transform: this.data('origTransform') + (this.data('origTransform') ? "T" : "t") + [dx, dy]
+//             });
+//         },
+//         function () {
+//             this.data('origTransform', this.transform().local);
+//         }
+//     );
+
+//     // Additional styles (optional)
+//     element.attr({ cursor: 'move' });
+// }
+
+
+let  addcolb = function(){Snap.load("/book.svg",function(f){
+    let book = f.select("#in"),
+        out = f.select("#out"),
+        colb1 = s.group(book,out)
+    s.append(colb1)
+    colb1.drag();
+    out.attr({
+        fill:"red"
+    })
+
+    out.animate({fill:"blue"},3000,mina.ease);
+
+}, s);
+}
+for(let i =0;i<3;i++){
+    addcolb();
+}   
 
 
 // Snap.load(img,function(f){
 //     g = f.selectAll("polygon");
 //     s.append(g);
 // })
-
-console.log(s);
